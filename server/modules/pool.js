35967ff967a430477f048e2370c8570927b65ff9
Awren Nuit <54560441+AwrenNuit@users.noreply.github.com>
2020-01-08 08:07:57 -0600

    updated pool for heroku deployment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,4 +1,35 @@
-const pg = require(`pg`);
+const pg = require('pg');
+const url = require('url');
+let config = {};
+
+if (process.env.DATABASE_URL) {
+    let params = url.parse(process.env.DATABASE_URL);
+    let auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
+
+} else {
+    config = {
+        user: process.env.PG_USER || null,
+        password: process.env.DATABASE_SECRET || null,
+        host: process.env.DATABASE_SERVER || 'localhost',
+        port: process.env.DATABASE_PORT || 5432,
+        database: process.env.DATABASE_NAME || '',
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
+}
+
+module.exports = new pg.Pool(config);const pg = require(`pg`);
 const Pool = pg.Pool;
 
 const config = {
